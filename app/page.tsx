8bbcf7c1032df3cf8da60a5caf25d9b88cd9957f
doodersrage <doodersrage@gmail.com>
2026-03-11 11:01:54 -0400

    draft mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
-// GOOD: Using Server Actions to handle mutations
-import { logout } from './actions'
+// page that fetches data
+import { draftMode } from 'next/headers'
  
-export default function Page() {
+async function getData() {
+  const { isEnabled } = await draftMode()
+ 
+  const url = isEnabled
+    ? 'https://draft.example.com'
+    : 'https://production.example.com'
+ 
+  const res = await fetch(url)
+ 
+  return res.json()
+}
+ 
+export default async function Page() {
+  const { title, desc } = await getData()
+ 
   return (
-    <>
-      <UserProfile />
-      <form action={logout}>
-        <button type="submit">Logout</button>
-      </form>
-    </>
+    <main>
+      <h1>{title}</h1>
+      <p>{desc}</p>
+    </main>
   )
 }
